ac4e503e390945c157449dbf897cc442257a9cd1
FScoward <endlick@gmail.com>
2019-08-26 00:11:56 +0900

    recursive crawler

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ response.xpath('/html/body/div/div/div[1]/div/div/div[1]/article/div[2]/div/div[
 
 # 投稿日を取得する
 response.xpath('/html/body/div/div/div[1]/div/div/div[1]/article/div[3]/ul/li/text()').extract()
+
+# 次へのリンクを取得する
+response.xpath('/html/body/div/div/div[1]/div/div/div[1]/div[1]/div[3]/div[2]/a/@href').get()
--- karin/spiders/blog.py
+++ karin/spiders/blog.py
@@ -1,28 +1,34 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from karin.items import KarinItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class BlogSpider(scrapy.Spider):
+class BlogSpider(CrawlSpider):
     name = 'blog'
     allowed_domains = ['www.keyakizaka46.com']
     start_urls = ['http://www.keyakizaka46.com/s/k46o/diary/detail/19313']
 
-    def parse(self, response: scrapy.http.TextResponse):
+    rules = (
+        Rule(LinkExtractor(restrict_xpaths='/html/body/div/div/div[1]/div/div/div[1]/div[1]/div[3]/div[2]/a'),
+             callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
         item = KarinItem()
         title = response.xpath('/html/body/div/div/div[1]/div/div/div[1]/article/div[1]/div[2]/h3/text()').get()
         posted_date = response.xpath('/html/body/div/div/div[1]/div/div/div[1]/article/div[3]/ul/li/text()').get()
         item['body'] = "".join(self.getResponseBody(response))
         item['title'] = self.remover(title)
         item['posted_date'] = self.remover(posted_date)
-        return item
+        yield item
 
     def getResponseBody(self, response) -> str:
-        body = response.xpath('/html/body/div/div/div[1]/div/div/div[1]/article/div[2]/div/div[2]/div[3]//text()')
+        body = response.xpath('/html/body/div/div/div[1]/div/div/div[1]/article/div[2]//text()')
         strBody = "".join(body.extract())
         return self.remover(strBody)
 
     @staticmethod
     def remover(target) -> str:
         return target.replace(u'\xa0', u"").strip().replace(u'\n', u'')
-
